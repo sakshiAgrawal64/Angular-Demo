93593c1f912392e4d469212e4ccb6f9125117e0e
Sakshi Agrawal <sakshigrawal64@gmail.com>
2020-06-14 23:45:57 +0530

    Welcome page

---
+++ my-app/src/app/components/example.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+
+@Component({
+
+    selector : 'app-example',
+    template : `<h1>Hello</h1>`
+})
+
+export class ExampleComponent {
+
+    
+}
